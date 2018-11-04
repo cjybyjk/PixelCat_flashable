ff36efc610245b6f4a39ecf3cbabb0830aa5957e
cjybyjk <cjybyjk@gmail.com>
2018-11-04 13:38:43 +0800

    update generator

--- generate/generate.sh
+++ generate/generate.sh
@@ -1,18 +1,59 @@
 #!/system/bin/sh
 
+# 可以自行添加 timer_rate
+allowed_timer_rate=(
+"boost"
+"boostpulse_duration"
+"timer_rate"
+"timer_slack"
+"min_sampling_time"
+"align_windows"
+"max_freq_hysteresis"
+"enable_prediction"
+"io_is_busy"
+"ignore_hispeed_on_notif"
+"use_sched_load"
+"use_migration_notif"
+"go_hispeed_load"
+"hispeed_freq"
+"above_hispeed_delay"
+"target_loads"
+)
+
+function trim()
+{
+    echo $1 | sed -e 's/^[ \t]*//g' -e 's/[ \t]*$//g'
+}
+
 function savemode()
 {
     if [ "" != "$modeText" ]; then
-        sed -i "s/$mode/$modeText/g" powercfg.apk
+        sed -i "s/${mode}_params/$modeText/g" powercfg.apk
         modeText=""
         echo "$mode saved"
     fi
 }
 
+function check_timer_rate()
+{
+    for i in ${!allowed_timer_rate[@]}
+    do
+        if [ "$timer_rate" = "${allowed_timer_rate[$i]}" ]; then
+            return 0
+        fi
+        if [[ "$timer_rate" =~ "${allowed_timer_rate[$i]}" ]] || [[ "${allowed_timer_rate[$i]}" =~ "$timer_rate" ]]; then
+            mostLikely=${allowed_timer_rate[$i]}
+        fi
+    done
+    echo "目标 \"$timer_rate\" 可能存在错误, 与它最相似的是 \"$mostLikely\""
+    read -p "请在此进行修改(留空则不修改):" rightTimerRate <&3
+    [ -z "$rightTimerRate" ] && rightTimerRate="$timer_rate"
+    timer_rate="$rightTimerRate"
+}
+
 basepath=$(cd `dirname $0`; pwd)/template
 
-echo -n "输入Soc型号:"
-read socModel
+read -p "输入Soc型号:" socModel
 if [ ! -f "$basepath/$socModel/powercfg.apk" ]; then
     echo "! 没有适配这个型号"
     exit 0
@@ -28,45 +69,49 @@ cp -r $basepath/$socModel/* ./
 vim ./perf_text
 
 # 对复制的调度进行处理
-echo -n "规范化调度参数(y/N):"
+echo -n "规范化调度参数(y/n):"
 read flag_TextReplace
-if [ "$flag_TextReplace" == "y" ]; then
+if [ "$flag_TextReplace" = "y" ]; then
+    sed -i 's/[A-Z]/\u&/g' ./perf_text
+    sed -i 's/:/：/g' ./perf_text
+    sed -i 's/： /：/g' ./perf_text
+    sed -i 's/：：/：/g' ./perf_text
     sed -i 's/\([0-9]\)：\([0-9]\)/\1:\2/g' ./perf_text
-    sed -i 's/align_ windows/align_windows/g' ./perf_text
-    sed -i 's/\([a-z]\):：/\1：/g' ./perf_text
-    sed -i 's/go_hispeed_freq：\([0-9]*\) \([0-9]*\)/go_hispeed_freq：\nbig：\1\nlittle：\2/g' ./perf_text
+    sed -i 's/big：/\nbig：/g' ./perf_text
+    sed -i 's/little：/\nlittle：/g' ./perf_text
+    sed -i '/^\s*$/d' ./perf_text
     echo -e "\n" >> ./perf_text
 fi
 
 # default
-mode="balance_params"
-modeTload="balance_tload"
+mode="balance"
 
 OLD_IFS="$IFS" 
 IFS="："
+
+# 备份标准输入
+exec 3<&0
+
 while read lineinText
 do 
-    [[ "$lineinText" == "" ]] && continue
+    [ -z "$lineinText" ] && continue
     if [[ "$lineinText" =~ "省电" ]]; then
         savemode
-        mode="powersave_params"
-        modeTload="powersave_tload"
+        mode="powersave"
         continue
     elif [[ "$lineinText" =~ "性能" ]]; then
         savemode
-        mode="performance_params"
-        modeTload="performance_tload"
+        mode="performance"
         continue
     elif [[ "$lineinText" =~ "均衡" ]]; then
         savemode
-        mode="balance_params"
-        modeTload="balance_tload"
+        mode="balance"
         continue
     fi
 
     arrCmd=($lineinText)
     timer_rate="${arrCmd[0]}"
-    if [ "$timer_rate" == "big" ] || [ "$timer_rate" == "little" ]; then
+    if [ "$timer_rate" = "big" ] || [ "$timer_rate" = "little" ]; then
         timer_rate=$timer_rate_bak
         cluster="${arrCmd[0]}" 
         param="${arrCmd[1]}"
@@ -80,8 +125,12 @@ do
             param="${arrCmd[2]}"
         fi
     fi
-    [[ "" == "$param" ]] && continue
-    [[ "$timer_rate" == "target_loads" ]] && [[ "$cluster" == "little" ]] && sed -i "s/$modeTload/$param/g" powercfg.apk
+    param=`trim "$param"`
+    cluster=`trim "$cluster"`
+    timer_rate=`echo $timer_rate | tr -d '[ \t]'`
+    [ -z "$param" ] && continue
+    check_timer_rate
+    [ "$timer_rate" = "target_loads" ] && [ "$cluster" = "little" ] && sed -i "s/${mode}_tload/$param/g" powercfg.apk
     [[ "$param" =~ " " ]] && param="\"$param\""
     modeText=${modeText}"set_param_$cluster $timer_rate $param\n	" 
 done < ./perf_text
